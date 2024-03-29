3777c68dbf8da332c60c9d44c648057a8960cde6
Tshenolo17 <ntshononohope@gmail.com>
2024-03-29 20:42:26 +0200

    initial setup and understanding

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents) // this is how we register a handler for http methods
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "data fectched successfully",
+	})
 }
