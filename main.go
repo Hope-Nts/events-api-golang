d762497513ae6b03231a00caeb4f319a34128b2a
Tshenolo17 <ntshononohope@gmail.com>
2024-04-04 00:28:12 +0200

    setup db, get and post endpoints

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"example.com/rest-api/db"
+	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents) // this is how we register a handler for http methods
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"message": "data fectched successfully",
-	})
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events, try again later"})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	fmt.Println(err)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not create event, try again later"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
---
+++ models/event.go
@@ -0,0 +1,66 @@
+package models
+
+import (
+	"time"
+
+	"example.com/rest-api/db"
+)
+
+type Event struct {
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int       //links the event to the user who created it
+}
+
+var events = []Event{}
+
+func (event Event) Save() error {
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id )
+	VALUES (?,?,?,?,?)
+	`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	event.ID = id
+	// later: add the event to the database
+	return err
+}
+
+// this is a normal function and not a method to the Event struct
+func GetAllEvents() ([]Event, error) {
+	var events []Event
+	query := `SELECT * FROM events`
+
+	rows, err := db.DB.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
