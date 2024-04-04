080432f2a07cda78ba65cc6ff07621e4c464b666
Tshenolo17 <ntshononohope@gmail.com>
2024-04-04 23:55:51 +0200

    events CRUD operations

--- routes/events.go
+++ routes/events.go
@@ -61,3 +61,61 @@ func createEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "there was an error parsing the id"})
+		return
+	}
+
+	_, err = models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "event not found"})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse requeest data"})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not update event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "event updated succesfully"})
+}
+
+func deleteEvent(context *gin.Context) {
+	// ?var event models.Event
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "error parsing event id"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event data"})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "error deleting event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "event deleted succesfully"})
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -6,4 +6,6 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents) // this is how we register a handler for http methods
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
+	server.PUT("/events/:id", updateEvent)
+	server.DELETE("/events/:id", deleteEvent)
 }
